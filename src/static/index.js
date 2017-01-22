7b4f257319087f986c9793bbe234d913967cb657
Fabian Beuke <mail@beuke.org>
2017-01-22 15:15:43 +0100

    simple elm js highcharts interop

--- src/static/index.js
+++ src/static/index.js
@@ -1,20 +1,69 @@
-// pull in desired CSS/SASS files
 require( './styles/main.scss' );
-var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if jQuery not needed
-require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' );   // <--- remove if Bootstrap's JS not needed
-
-// inject bundled Elm app into div#main
 require('./data/gh-star-event.json')
+
 var Elm = require( '../elm/Main' );
 var main = Elm.Main.embed( document.getElementById( 'main' ) );
 
-// Elm.fullscreen(Elm.Main, {reset:[]});   // take over the whole page
-// var div = document.getElementById('elm-main');
-// var main = Elm.embed(Elm.Main, div, {reset:[]});
+var options = {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        credits: {
+            enabled: false
+        },
+        title: {
+            text: 'Browser market shares January, 2015 to May, 2015'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Microsoft Internet Explorer',
+                y: 56.33
+            }, {
+                name: 'Chrome',
+                y: 24.03,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Firefox',
+                y: 10.38
+            }, {
+                name: 'Safari',
+                y: 4.77
+            }, {
+                name: 'Opera',
+                y: 0.91
+            }, {
+                name: 'Proprietary or Undetectable',
+                y: 0.2
+            }]
+        }]
+    };
 
-// You can send and receive values through
-// ports 'reset' and 'count'.
-// main.ports.reset.send([]);
+Highcharts.chart('container', options);
+
 main.ports.check.subscribe(function(event) {
-    console.log(event);
+        console.log(event);
+        options.title.text=event;
+        Highcharts.chart('container', options);
 });
